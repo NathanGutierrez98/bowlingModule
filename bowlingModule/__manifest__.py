ad5a9832806fce5fc742c78f7182021973adb1c4
NathanGutierrez98 <nathangut98@gmail.com>
2019-02-28 18:02:00 +0100

    Create __manifest__.py

---
+++ bowlingModule/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "bowling_module",
+
+    'summary': """
+        Bowling Alley management""",
+
+    'description': """
+        Bowling-Module es un módulo de Odoo que nos permite gestionar las pistas de nuestra bolera
+        mantenimiento de estas, registro de partidas, usuarios... 
+    """,
+
+    'author': "Nathan Gutiérrez Gil",
+    'website': "nathangut98@gmail.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Other',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/data.xml'
+
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
